9bca3ed2ccdcc6a62a04bcdcb69754ef36ec6e0d
MohammadUmar89 <umarsattar1989@gmail.com>
2023-03-27 13:32:41 +0500

    Initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: Testing
+ * Plugin URI: https://localhost.com/
+ * Description: TEsting for Github autoupdate
+ * Author: Cyberhawks
+ * Author URI: https://cyberhawks.com/
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
