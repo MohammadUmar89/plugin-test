1037e92fcc90dc3ca423d385f2646e135cdbabe5
MohammadUmar89 <umarsattar1989@gmail.com>
2023-03-28 15:16:01 +0500

    Version Updated

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ Plugin URI:  http://localhost/
 Author:      CyberHawks
 Author URI:  https://cyberhawks.com/
 Description: PLUGIN DESCRIPTION
-Version:     0.9.8
+Version:     0.9.9
 Text Domain: text-domain
 GitHub Plugin URI: MohammadUmar89/plugin-test
 GitHub Plugin URI: https://github.com/MohammadUmar89/plugin-test.git
  */
 
- echo "This is new";
+ echo "0.9.9 Version Update";
